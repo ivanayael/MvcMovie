7cfb593b5c33d7ffa72cb95a212f4d295585ea29
Ivana Yael Curra <icurra@quadiontech.com>
2020-07-16 17:57:13 -0300

    update code

---
+++ projectcode/MvcMovie/MvcMovie/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //public IActionResult About()
+        // {
+        //    ViewData["Message"] = "Your application description page.";
+
+          //  return View();
+       // }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            var viewModel = new Address()
+            {
+                Name = "Microsoft",
+                Street = "One Microsoft Way",
+                City = "Redmond",
+                State = "WA",
+                PostalCode = "98052-6399"
+            };
+
+            return View(viewModel);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
